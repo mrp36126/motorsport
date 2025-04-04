ea5191d1566c0c86aff68c981ef55a079f4d216d
mrp36126 <mrp36126@gmail.com>
2025-04-04 14:44:31 +0200

    Update sport.js

--- sport.js
+++ sport.js
@@ -1,27 +1,10 @@
 // sport.js
 
 function displayRugbySchedules() {
-    displayInternationalRugbySchedule();
-    displayURCSchedule();
-}
-
-function displayInternationalRugbySchedule() {
     const scheduleList = document.getElementById("intRugby-schedule");
     if (scheduleList && schedules.intRugby) {
         scheduleList.innerHTML = "";
         schedules.intRugby.forEach(event => {
-            const li = document.createElement("li");
-            li.textContent = `${event.date} - ${event.event} at ${event.venue} (${event.time})`;
-            scheduleList.appendChild(li);
-        });
-    }
-}
-
-function displayURCSchedule() {
-    const scheduleList = document.getElementById("urc-schedule");
-    if (scheduleList && schedules.urc) {
-        scheduleList.innerHTML = "";
-        schedules.urc.forEach(event => {
             const li = document.createElement("li");
             li.textContent = `${event.date} - ${event.event} at ${event.venue} (${event.time})`;
             scheduleList.appendChild(li);
